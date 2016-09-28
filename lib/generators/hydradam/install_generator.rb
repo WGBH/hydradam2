fedb1ff6f2ca42566058445adbee0792d111a667
Andrew Myers <afredmyers@gmail.com>
2016-09-28 14:33:31 -0600

    Return FileSets in search results
    Does so by injecting Hydradam::SearchBuilderBehavior during installation of
    Hydradam gem.
    
    Also added a pending integration tests that should be completed later, but
    don't want to block on it right now.
    
    Closes HDM-560.

--- lib/generators/hydradam/install_generator.rb
+++ lib/generators/hydradam/install_generator.rb
@@ -1,6 +1,10 @@
 module Hydradam
   class InstallGenerator < Rails::Generators::Base
 
-
+    def inject_search_builder_behavior
+      inject_into_file 'app/models/search_builder.rb', after: "include CurationConcerns::SearchFilters\n" do
+        "\tinclude Hydradam::SearchBuilderBehavior\n"
+      end
+    end
   end
 end
